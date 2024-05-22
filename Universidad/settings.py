e3f6da982626916ceb59aef8066d5432fc9926f4
_damiancortes <damian.cortes@usm.cl>
2024-05-22 18:32:28 -0400

    Update settings.py

--- Universidad/settings.py
+++ Universidad/settings.py
@@ -33,7 +33,6 @@ LOGOUT_REDIRECT_URL = '/'
 # Applpyication definition
 
 INSTALLED_APPS = [
-    'admin_interface',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,10 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'colorfield',
 ]
-
-X_FRAME_OPTIONS='SAMEORIGIN'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
