29bfcbbd2fc1be3542c4401515e127380e2b95b2
benja14slq <benjamin.betty@sansano.usm.cl>
2024-05-21 23:34:31 -0400

    Página Registro

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, registro
 
 urlpatterns = [
     path('', home, name = "home"),
+    path('registro/', registro, name="regsitro")
 ]
--- core/views.py
+++ core/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'core/home.html')
 
+def registro(request):
+    return render (request, 'registration/registro.html')
 # Create your views here.
